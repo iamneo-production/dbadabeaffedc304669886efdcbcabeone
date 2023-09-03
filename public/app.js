35522a40d74d969e49db5efb0eed0a65af2dbe7d
Iamneo <techadmin@iamneo.ai>
2023-09-03 07:06:55 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -24,7 +24,7 @@ const ticTacToe = (element, index) => {
         element.innerText = currentPlayer;
 
         if (checkWin()) {
-            result.innerText = `Player ${currentPlayer} won`;
+            result.innerText = `Player ${currentPlayer} Won 🎉`;
             gameActive = false;
         } else if (cells.every(cell => cell !== '')) {
             result.innerText = "It's a draw!";
